ad3907bf5e225650e8a1c87a179866c8a30eee88
karen <karentlk1920@gmail.com>
2019-09-04 17:39:02 +0800

    working on getpost

--- src/Profile/components/MyPostscomponents/BothPost.vue
+++ src/Profile/components/MyPostscomponents/BothPost.vue
@@ -14,14 +14,14 @@
                 <acceptrejectcard/>
 
             </vs-collapse-item>
-            
+            <p>{{bookingParents}}</p>
             <vs-collapse-item>
-                <div slot="header" style="border-bottom: inset; display: flex; background-color: rgba(7,104,159,0.75); border-radius: 9px  ">
-                    <img style="width: 30%; height:30%; display: flex; padding: 9px;" src="https://www.generationsforpeace.org/wp-content/uploads/2018/03/empty.jpg"/>
+                <div slot="header" style="border-bottom: inset; display: flex; background-color: rgba(7,104,159,0.75); border-radius: 9px ">
+                    <img style="width: 30%; height:30%; display: flex; padding: 9px;" :src="bookingParents.post_img"/>
                     <div style="width: 70%; display: flex; padding: 9px; flex-wrap: wrap; font-family: 'Pavanam', sans-serif;">
-                        <p style="width: 100%; margin: 1px">Name: offering</P>
-                        <p style="width: 100%; margin: 1px">Speciality: {{speciality}}</P>
-                        <p style="width: 100%; margin: 1px">Price: {{price}}</P>
+                        <p style="width: 100%; margin: 1px">Name: {{bookingParents.type}}</P>
+                        <p style="width: 100%; margin: 1px">Speciality: {{bookingParents.category}}</P>
+                        <p style="width: 100%; margin: 1px">Price: {{bookingParents.price}}</P>
                     </div>
                 </div>
                 <p style="width: 100%; margin: 1px; margin-left: 10px">Status: {{status}}</P>
@@ -33,7 +33,7 @@
 
 <script>
 import acceptrejectcard from '@/Profile/components/MyPostscomponents/AcceptRejectCard.vue'
-
+const axios = require("axios");
 
 export default {
     name:'bothpost',
@@ -43,7 +43,9 @@ export default {
     },
     data(){
         return{
-            posts: Array,
+            posts: [],
+            bookingParents: [],
+            test: {},
             name:'',
             speciality:'123123',
             price:'123',
@@ -53,15 +55,47 @@ export default {
         }
     },
     methods: {
-        post() {
+        postByUser() {
             return axios.get(`http://127.0.0.1:3333/user/${this.userId}/post`)
         },
+
+        bookingByUser() {
+            return axios.get(`http://127.0.0.1:3333/user/3/bookingPending`)
+        },
+
+        bookingParent(pid) {
+            return axios.get(`http://127.0.0.1:3333/onePost/${pid}/`)
+        },
+
         async getPost() {
-            let posts = await this.post()
+            let posts = await this.postByUser()
+            this.posts = posts
+
+        },
+
+        async getBooking() {
+            let response = await this.bookingByUser()
+            let bookings = response.data
+            for (let i = 0; i < bookings.length; i++) {
+                // this.test = bookings[i]
+                let pid = bookings[i].post_id
+                let bookingParent = await this.bookingParent(pid)
+                bookingParent['booking'] = bookings[i]
+                console.log(bookingParent)
+                this.bookingParents.push(bookingParent)
+                //check if bookingparent is in parent array
+                // if (parents.includes(bookingParent)) {
+                //     //extract the parent and add the booking to the parent
+                // }
+                // else {
+                //     parents.push(bookingParent)
+                // }
+            } 
         }
     },
     mounted() {
         this.userId = localStorage.getItem('userId')
+        this.getBooking()
     }
 
 }
